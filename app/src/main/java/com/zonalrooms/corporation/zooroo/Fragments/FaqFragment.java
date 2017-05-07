8cbbc751b73330a09eca694941cff7cd2bd81a6b
Aman Kumar Gupta <amangupta15jan@gmail.com>
2017-05-07 16:59:08 +0530

    https://github.com/darkshadow237/ZooRoo

---
+++ app/src/main/java/com/zonalrooms/corporation/zooroo/Fragments/FaqFragment.java
@@ -0,0 +1,8 @@
+package com.zonalrooms.corporation.zooroo.Fragments;
+
+/**
+ * Created by DELL on 5/7/2017.
+ */
+
+public class FaqFragment {
+}
---
+++ app/src/main/java/com/zonalrooms/corporation/zooroo/LegalPolicyActivity.java
@@ -0,0 +1,8 @@
+package com.zonalrooms.corporation.zooroo;
+
+/**
+ * Created by DELL on 5/7/2017.
+ */
+
+public class LegalPolicyActivity {
+}
